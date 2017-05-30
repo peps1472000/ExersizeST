c97a60613d842bcdbcb7870cb18151a6194f79d2
Jack <darkangelhackerjack@gmail.com>
2017-05-30 08:16:42 -0700

    obj\Debug\main.o main.cpp bin\Debug\GameShop.exe GS.layout GS.cbp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+double Priceing();
+
+int main()
+{
+    double a;
+    cout << "Nice to meet you. Please look around our shop. And take your time."<< endl;
+    cout << "Here are genres and the exit(leading toward the final step of paying)." << endl;
+    cout << "Enter the wanted number and then enter \n\n";
+    a = Priceing();
+    cout << a << "$";
+}
+double Priceing(){
+    int a, b, sum;
+    cout << "Genres: \n 1)RPG \n 2)FPS \n 3)Cars \n 4)Anime \n 5)EXIT \n\n";
+    cin >> a;
+    if(a == 1)
+        {cout << "Something";}
+        if else(a==2)
+    return sum;
+}
