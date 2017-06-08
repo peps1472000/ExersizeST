0f909f0e7bcccb9cc4968867fbea33ecf820aa0e
LinleyYT <darkangelhackerjack@gmail.com>
2017-06-08 22:36:39 +0300

    Lab\Program.cs

---
+++ Lab/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class PriceChangeAlert
+{
+    static void Main()
+    {
+        FibunacchiNum(long.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+    }
+
+    private static void FibunacchiNum(long times, int range)
+    {
+        long[] number = new long[times];
+        number[number.Length - 1] = 1;
+        FibunacchiNumMath(number, range, times);
+    }
+    static void FibunacchiNumMath(long[] number, int range, long n)
+    {
+
+        for (int i = 0; i < n; i++)
+        {
+            long sum = 0;
+            for (int j = number.Length - range; j < number.Length; j++)
+            {
+                sum += number[j];
+
+            }
+            if (i!=n-1)
+            {
+                for (int k = 1; k < number.Length; k++)
+                {
+                    number[k - 1] = number[k];
+                }
+                number[number.Length - 1] = sum;
+            }
+        }
+
+            foreach (long item in number)
+            {
+                Console.WriteLine(item +" ");
+            }
+    }
+}
